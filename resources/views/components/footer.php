b0b27f0f0fb72714f8ed220fc5d397c60625bb61
MiftahulHuda223040145 <heromiftah2004@gmail.com>
2025-02-15 22:27:02 +0700

    Slicing Navbar, Footer and Home

---
+++ resources/views/components/footer.php
@@ -0,0 +1,28 @@
+<footer class="bg-[#B82132]">
+<!-- Footer -->
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div class="grid grid-cols-3 my-10 gap-0">
+            <div class="text-justify">
+                <h2 class="text-xl font-bold text-white">FrontendLab adalah website menyediakan wadah untuk membuat sebuah tutorial membuat website. Anda  dapat menemukan panduan praktis, tips, dan best practice dalam membangun antarmuka web yang interaktif dan responsif.</h2>
+            </div>
+            <div class="flex justify-center items-center ">
+                <a href="#">
+                    <img src="../img/logo.png" class="lg:w-[200px]" alt="Logo FrontendLab">
+                </a>
+            </div>
+            <div>
+                <ul class="text-xl text-white">
+                    <li class="mb-3 font-bold"><a href="#">Informasi</a></li>
+                    <li class="mb-3"><a href="#">Tentang Kami</a></li>
+                    <li class="mb-3"><a href="#">Panduan dan Keamanan</a></li>
+                    <li class="mb-3"><a href="#">Customer Service</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+<!-- Copyright -->
+    <div class="bg-[#141414]">
+        <p class="text-white font-bold text-center py-3">Copyright - FrontendLab 2025</p>
+    </div>
+</footer>
--- resources/views/layouts/home.layout.php
+++ resources/views/layouts/home.layout.php
@@ -4,14 +4,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../resources/css/global.css">
-  <title>FrontendLab - <?= $data['judul'] ?></title>
+  <link rel="stylesheet" href="../css/global.css">
+  <title>FrontendLab - Home</title>
 </head>
 
-<body>
-  <?php include "../resources/views/components/navbar.php" ?>
+<body class="bg-gray-100">
+  <?php include "../views/components/navbar.php" ?>
 
   <script src="../node_modules/flowbite/dist/flowbite.min.js"></script>
 </body>
-
 </html>
